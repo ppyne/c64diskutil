120d34dff7c8ec78749c810463f3c40e6c74e6c7
Parker Pyne <parker.pyne.detective@gmail.com>
2022-08-16 18:44:04 +0200

    Adding VLIR record ID which is different from the record index

--- c64diskgeosrecord.cpp
+++ c64diskgeosrecord.cpp
@@ -8,7 +8,7 @@ C64DiskGeosRecord::C64DiskGeosRecord(C64DiskSector *parent, char *data) : C64Dis
 void C64DiskGeosRecord::populateRecords(QList<GeosRecordPointer> &records) {
     int id = 0;
     for (int i = 0x02; i <= 0xFE; i = i + 0x02) {
-        qDebug() << "VLIR Record" << id << (unsigned char)this->Data[i] << (unsigned char)this->Data[i + 1];
+        //qDebug() << "VLIR Record" << id << (unsigned char)this->Data[i] << (unsigned char)this->Data[i + 1];
         if ((unsigned char)this->Data[i] == 0 && (unsigned char)this->Data[i + 1] == 0) break;
         if ((unsigned char)this->Data[i] != 0 && (unsigned char)this->Data[i + 1] != 0xFF)
             records.append(GeosRecordPointer((unsigned char)this->Data[i], (unsigned char)this->Data[i + 1], id));
